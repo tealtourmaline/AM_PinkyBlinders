33f7354af72c0d227ff1738a6c97406d9d8d01d3
Val Espinal <valeria.espinal@upb.edu.co>
2024-10-07 19:57:34 -0500

    creación y corrección de errores vista detalle de entrada

---
+++ app/src/main/java/co/edu/upb/pinkyblinders/ui/EntryDetailScreen.kt
@@ -0,0 +1,125 @@
+package co.edu.upb.pinkyblinders.ui.theme
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import co.edu.upb.pinkyblinders.R
+
+@Composable
+fun EntryDetailScreen() {
+    Scaffold {
+        EntryDetailBodyContent()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryDetailBodyContent() {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFE4FA)),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        // Navbar
+        Navbar()
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Fecha
+        Text(
+            modifier = Modifier.padding(start = 16.dp),
+            text = "Fecha/de/entrada",
+            color = Color(0xFFF61067),
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        //Título
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween // Espacio entre el título y los iconos
+        ) {
+            // Título
+            Text(
+                text = "Título de la entrada",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = LobsterTwoFont,
+                color = Color(0xFFF61067)
+            )
+
+            // Iconos de editar y eliminar
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.editar),
+                    contentDescription = "Editar",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clickable {
+                            // pendiente: programar el editar
+                        }
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.eliminar),
+                    contentDescription = "Eliminar",
+                    modifier = Modifier
+                        .size(30.dp) //
+                        .clickable {
+                            // pendiente: programar el eliminar
+                        }
+                )
+            }
+        }
+
+        //Texto de la entrada
+        Text(
+            modifier = Modifier.padding(25.dp),
+            text = "Este es un ejemplo de una entrada de diario. Aquí puedes escribir todo lo que te ha sucedido hoy, tus pensamientos y cómo te has sentido a lo largo del día. Este espacio está diseñado para que puedas reflexionar sobre tus experiencias, grandes o pequeñas, y sobre cómo estas te han impactado. Tal vez tu día ha sido tranquilo y has disfrutado de pequeños momentos de paz, o quizás ha sido más agitado, lleno de retos y emociones intensas. No importa qué tipo de día hayas tenido, este es un lugar seguro para desahogarte y expresar todo lo que llevas dentro.\n" +
+                    "\n" +
+                    "Este diario también puede ser un buen lugar para reflexionar sobre tus metas, lo que te gustaría alcanzar en los próximos días o semanas, y cómo te sientes con respecto a tu progreso. Tómate el tiempo para escribir con calma, sin prisa, y aprovecha este momento para conectar contigo mismo.",
+            fontWeight = FontWeight.Normal,
+            fontSize = 19.sp,
+        )
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EntryDetailScreenPreview() {
+    EntryDetailScreen()
+}
