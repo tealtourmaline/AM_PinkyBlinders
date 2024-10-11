f1ac7e4bea901a7c15cbe11b9d7dcd8e4c453a04
tealtourmaline <softivyad@gmail.com>
2024-10-10 21:27:51 -0500

    MainScreen y NavbarMenu

--- app/src/main/java/co/edu/upb/pinkyblinders/navigation/AppNavigation.kt
+++ app/src/main/java/co/edu/upb/pinkyblinders/navigation/AppNavigation.kt
@@ -12,31 +12,29 @@ import co.edu.upb.pinkyblinders.ui.theme.LoginScreen
 import co.edu.upb.pinkyblinders.ui.theme.NewEntryScreen
 
 @Composable
-fun AppNavigation(){
+fun AppNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = AppScreens.SplashScreen.route){
-        composable(route = AppScreens.SplashScreen.route){
+    NavHost(navController = navController, startDestination = AppScreens.SplashScreen.route) {
+        composable(route = AppScreens.SplashScreen.route) {
             SplashScreen()
         }
-        composable(route = AppScreens.RegisterScreen.route){
+        composable(route = AppScreens.RegisterScreen.route) {
             RegisterScreen()
         }
-        composable(route = AppScreens.NewEntryScreen.route){
+        composable(route = AppScreens.NewEntryScreen.route) {
             NewEntryScreen()
         }
-        composable(route = AppScreens.MainScreen.route){
+        composable(route = AppScreens.MainScreen.route) {
             LoginScreen()
         }
-        composable(route = AppScreens.LoginScreen.route){
+        composable(route = AppScreens.LoginScreen.route) {
             LoginScreen()
         }
-        composable(route = AppScreens.EntryDetailScreen.route){
+        composable(route = AppScreens.EntryDetailScreen.route) {
             EntryDetailScreen(null, null, null)
         }
-        composable(route = AppScreens.CreditsScreen.route){
-            Credits()
-        }
-        composable(route = AppScreens.ConfigScreen.route){
+        composable(route = AppScreens.ConfigScreen.route) {
             ConfigScreen()
+        }
     }
 }
--- app/src/main/java/co/edu/upb/pinkyblinders/ui/MainScreen.kt
+++ app/src/main/java/co/edu/upb/pinkyblinders/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package co.edu.upb.pinkyblinders.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.border
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -20,8 +21,108 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.edu.upb.pinkyblinders.R
+import co.edu.upb.pinkyblinders.ui.theme.LobsterTwoFont
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(){
+    Scaffold(){
+        MainScreenBodyContent()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreenBodyContent(){
+    var nombre by remember { mutableStateOf("") }
+    var pin by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFE4FA))
+    ) {
+        NavbarMenu()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Mis entradas",
+                textAlign = TextAlign.Center,
+                fontSize = 35.sp,
+                fontWeight = FontWeight(400),
+                fontFamily = LobsterTwoFont,
+                color = Color(0xFFF61067)
+
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            for (i in 1..6) {
+                EntryCard("Título $i", "Fecha/de/creación")
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Image(
+                painter = painterResource(R.drawable.editar),
+                contentDescription = "Edit Icon",
+                modifier = Modifier
+                    .size(36.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun EntryCard(title: String, creationDate: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(76.dp),
+        shape = RoundedCornerShape(21.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFFFC4EB)) // Color de fondo rosa
+    ) {
+        Row(
+            modifier = Modifier.padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black
+                    )
+                )
+                Text(
+                    text = creationDate,
+                    style = TextStyle(
+                        fontSize = 23.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = Color.Black
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
+}
